06f4ee14ebcd63a37df342ac985d3e1e3bee1f23
Jhoan Daner <jhoandaner2005@gmail.com>
2026-04-13 13:51:54 -0500

    feat: actualizacion de vista; se cre'o tabla para mejor visualizacion

--- frontend/src/views/AlertasView.vue
+++ frontend/src/views/AlertasView.vue
@@ -14,8 +14,7 @@
       </thead>
 
       <tbody>
-        <tr v-for="producto in alertas">
-          :key="producto.id">
+        <tr v-for="producto in alertas" :key="producto.id">
           <td>{{ producto.nombre }}</td>
           <td>{{ producto.categoria }}</td>
           <td style="color: red; font-weight: bold">{{ producto.stock_actual }}</td>
@@ -38,3 +37,31 @@ onMounted(async () => {
   alertas.value = response.data;
 });
 </script>
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+th,
+td {
+  border: 1px solid #ddd;
+  padding: 10px;
+  text-align: left;
+}
+
+th {
+  background-color: #2c3e50;
+  color: white;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+tr:hover {
+  background-color: #e8f4f8;
+}
+</style>
